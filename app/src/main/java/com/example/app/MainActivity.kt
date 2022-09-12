5d8e3ea9d9ad64a6970a9fdf77b0ee369399f177
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-09-12 23:11:47 +0900

    섹션 5 : 58번째 강의, Fragment

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate")
     }
 
