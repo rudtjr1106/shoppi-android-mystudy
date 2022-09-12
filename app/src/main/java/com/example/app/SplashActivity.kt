e32b07503be5a311ad0f769e7d5b2093b4c4cdd8
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-09-12 22:46:15 +0900

    55번째 강의, 스플래시 화면 구현 & 앱 호환성

---
+++ app/src/main/java/com/example/app/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
